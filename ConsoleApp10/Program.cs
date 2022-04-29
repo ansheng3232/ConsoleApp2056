84bfd1191e75d2101e08d8c1159492b65c8299a2
Chen An <v-chenan1@microsoft.com>
2022-04-29 02:07:30 +0000

    1

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-//12312468
-//123123456
-//mmm456
-//mmm1231231234556
-//mmm123123
-//mmm123456456
