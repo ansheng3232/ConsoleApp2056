5aa58909c4ad85f9f852ea258802cddc69c76a05
Chen An <v-chenan1@microsoft.com>
2022-05-09 07:08:06 +0000

    3

--- ConsoleApp10/Program - Copy (3).cs
+++ ConsoleApp10/Program - Copy (3).cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//4153
+#include <stdio.h>
+int main()
+{
+    int in ;
+    float i = 0 ,key, sum =0;
+	while(true){/*死循环*/
+	    scanf("%d",&in);
+	    if (in == -1){/*对输入的值进行判断*/
+	        break;/*跳出while循环*/
+	    }
+	    sum = sum + in;
+	        i = i + 1;
+	}
+	if (i != 0){/*对计数i的数值进行判断,如果有则输出平均数key，如果没有数据（指第一个数据就输入了-1）则输出无数据*/
+	    key = sum/i;
+	    printf("%.2f",key);
+	}
+	else{
+	    printf("无数据");
+	}
+return 0;
+}
