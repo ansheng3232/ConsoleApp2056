151e4676c0947ceea913a051e8d4868413ad798e
Chen An <v-chenan1@microsoft.com>
2022-05-12 09:33:15 +0000

    1

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -3,3 +3,47 @@ Console.WriteLine("Hello, World!");
 //test remote
 //local
 //7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
+Console.WriteLine("Hello, World!");
+//test remote
+//local
+//7894
